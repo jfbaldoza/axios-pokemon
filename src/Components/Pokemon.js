bd3aa15a56529e500d4360ba11899201bc384fac
Jan Francis Baldoza <baldozajan@gmail.com>
2023-01-19 08:50:46 -0500

    added pokemon component

---
+++ src/Components/Pokemon.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+
+function Pokemon() {
+
+    const [pokemonData, setPokemonData] = useState([])
+
+    useEffect(() => {
+        axios
+            .get("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
+            .then((res) => {
+                setPokemonData(res.data.results);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+            console.log("Worked");
+        }, []);
+
+    return (
+    <div>
+        <h1>The Pokedex</h1>
+        {
+            pokemonData.map((element, i) => {
+                return <p key={i}>{element.name}</p>
+            })
+        }
+    </div>
+    )
+}
+
+export default Pokemon
